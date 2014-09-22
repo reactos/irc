7c4d70a504cc1235327c0dacd0e454fba859e888
Sylvain Petreolle <spetreolle@yahoo.fr>
2014-09-22 13:31:14 +0000

    [TECHBOT] Bugcheck: The !bc command now displays the symbolic name of the given bugcheck code, in hexadecimal or digits. Answers both in private and public. It uses the bugcodes file from svn HEAD.
    svn path=/trunk/irc/; revision=2142

--- TechBot/TechBot.Commands.Common/BugcheckCommand.cs
+++ TechBot/TechBot.Commands.Common/BugcheckCommand.cs
@@ -1,27 +1,129 @@
 using System;
 using System.Xml;
+using System.Data;
 
 using TechBot.Library;
+using System.Net;
+using System.IO;
+using System.Globalization;
 
 namespace TechBot.Commands.Common
 {
-	[Command("bc", Help = "!bc <value>", Description = "Get the name of the given bugcheck code")]
-	public class BugcheckCommand : Command
-	{
-		public BugcheckCommand ()
-		{
-		}
-		public override void ExecuteCommand()
-		{
-            if (string.IsNullOrEmpty(Parameters))
+    [Command("bc", Help = "!bc <value>", Description = "Get the symbolic name of the given bugcheck code")]
+    public class BugcheckCommand : Command
+    {
+        object Lock = new object();
+        DataTable Bugs;
+
+        const string MessageId = "MessageId";
+        const string SymbolicName = "SymbolicName";
+        const string Severity = "Severity";
+
+        public BugcheckCommand()
+        {
+            Bugs = new DataTable();
+            Bugs.Columns.Add(MessageId, typeof(ulong));
+            Bugs.Columns.Add(SymbolicName, typeof(string));
+        }
+        public override void ExecuteCommand()
+        {
+            ulong BugCode;
+            if(!TryParseHex(Parameters, out BugCode))            
             {
                 Say("Please provide a valid BUGCHECK value.");
             }
-            else
+            else if (Bugs.Rows.Count == 0)
             {
                 Say("KebugCheckEx( PHASE0_INITIALIZATION_FAILED, NO_PAGES_AVAILABLE, MORAL_EXCEPTION_ERROR, DATA_COHERENCY_EXCEPTION );");
-			}
-		}
-	}
-}
+            }
+            else
+            {
+                DataRow[] Found = Bugs.Select(MessageId+"=" + BugCode);
+                if (Found.Length == 1)
+                    //Say(Parameters + " is " + Found[0]["SymbolicName"].ToString());
+                    Say("KeBugcheck( " + Found[0]["SymbolicName"].ToString() + " );");
+                else
+                    Say("KeBugcheck( " + Parameters + " ) not found.");
+            }
+        }
 
+        public override void Initialize()
+        {
+            base.Initialize();
+            lock (Lock)
+            {
+                if (Bugs.Rows.Count == 0)
+                    FetchCodes();
+            }
+        }
+        /// <summary>
+        /// Download bug codes from reactos server and parse it into a DataTable.
+        /// </summary>
+        private void FetchCodes()
+        {
+            WebClient Client = new WebClient();
+            string FileName;
+            try
+            {
+                FileName = Path.GetTempFileName();
+                Client.DownloadFile(Settings.Default.Bugcodes, FileName);
+            }
+            catch (IOException)
+            {
+                return;
+            }
+            using (StreamReader sr = File.OpenText(FileName))
+            {
+                string s = "";
+                string[]words;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    words = s.Split('=');
+                    switch (words[0])
+                    {
+                        case MessageId:
+                            DataRow Row = Bugs.NewRow();                            
+                            ulong Code = 0;
+                            if (!TryParseHex(words[1], out Code))
+                                continue;
+                            Row[MessageId] = Code;
+                            while ((s = sr.ReadLine()) != null)
+                            {
+                                words = s.Split('=');
+                                if (words[0] == Severity && words[1] != "Success")
+                                    break;
+                                if (words[0] == ".")
+                                {
+                                    Bugs.Rows.Add(Row);
+                                    break;
+                                }
+                                if (words[0] == SymbolicName)
+                                    Row[SymbolicName] = words[1];
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Parse a ulong/hexadecimal string to its ulong equivalent.
+        /// </summary>
+        /// <param name="s">A string representing the number to convert.</param>
+        /// <param name="result">Contains the result if successfully converted.</param>
+        /// <returns></returns>
+        static bool TryParseHex(string s, out ulong result)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                result = 0;
+                return false;
+            }
+            s = s.Trim();
+            const string Hex = "0x";
+            if (!s.StartsWith(Hex))
+                return ulong.TryParse(s, out result);
+            s = s.Substring(2);
+            return ulong.TryParse(s, NumberStyles.HexNumber, null, out result);
+        }
+    }
+}
